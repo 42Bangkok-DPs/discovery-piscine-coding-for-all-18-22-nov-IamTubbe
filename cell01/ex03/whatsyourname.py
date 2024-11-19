645a249210424980086649a2ab25c544af7a25d8
IamTubbe <67030298@kmitl.ac.th>
2024-11-19 23:11:42 +0700

    up

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
 text = input("Hey, what's your first name? : ").strip()
-text2 = (input("And your last name? : ")).strip()
+text2 = input("And your last name? : ").strip()
 print(f"Well , pleased to meet you , {text} {text2}.")
