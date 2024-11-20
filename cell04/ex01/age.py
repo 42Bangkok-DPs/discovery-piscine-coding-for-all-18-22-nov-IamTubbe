d4d6bc312d498ff935ac5a64c3ce9f6a690b661c
IamTubbe <67030298@kmitl.ac.th>
2024-11-20 16:19:38 +0700

    up

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 age_count = int(input("Please tell me your age: "))
 print(f"You are currently {age_count} year old.")
-age_plus1 = age_count + 10
-age_plus2 = age_count + 20
-age_plus3 = age_count + 30
-print(f"In 10 years. you'll be {age_plus1} years old.")
-print(f"In 20 years. you'll be {age_plus2} years old.")
-print(f"In 30 years. you'll be {age_plus3} years old.")
+print(f"In 10 years, you'll be {age_count + 10} years old.")
+print(f"In 20 years, you'll be {age_count + 20} years old.")
+print(f"In 30 years, you'll be {age_count + 30} years old.")
