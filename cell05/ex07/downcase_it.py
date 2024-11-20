519c0c09e1dadf438dd20ac522b7a5f3b92e2f3e
IamTubbe <67030298@kmitl.ac.th>
2024-11-21 00:18:17 +0700

    up

--- cell05/ex07/downcase_it.py
+++ cell05/ex07/downcase_it.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-# ตรวจสอบจำนวนพารามิเตอร์
 if len(sys.argv) == 2:
-    # แปลงพารามิเตอร์แรกเป็นตัวพิมพ์ใหญ่
     print(sys.argv[1].lower())
 else:
-    # แสดง "none" ถ้าพารามิเตอร์ไม่เท่ากับ 1
     print("none")
