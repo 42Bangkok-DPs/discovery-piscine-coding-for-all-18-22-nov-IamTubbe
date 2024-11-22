36facc346772ec59ce098a169fbe012e8fd08572
IamTubbe <67030298@kmitl.ac.th>
2024-11-22 18:21:08 +0700

    up

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
-# ลบ argument แรก (ชื่อไฟล์โปรแกรมเอง)
 arguments = sys.argv[1:]
-
-# นับจำนวน arguments ที่เหลือ
 num_parameters = len(arguments)
 
 print(f"Number of parameters: {num_parameters}")
--- cell05/ex05/aff_first_param.py
+++ cell05/ex05/aff_first_param.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python3
 import sys
-
-# รับพารามิเตอร์ทั้งหมด ยกเว้นชื่อไฟล์
 arguments = sys.argv[1:]
 
 if len(arguments) > 0:
-    # แสดงพารามิเตอร์ตัวแรก
     print(arguments[0])
 else:
-    # หากไม่มีพารามิเตอร์ แสดงคำว่า "none"
     print("none")
